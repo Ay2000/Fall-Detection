b0a6103f03309c6e6333fc643e13909cd12525ab
Ay2000 <45182653+Ay2000@users.noreply.github.com>
2018-11-19 15:32:43 -0800

    Update main test.cpp

--- main test.cpp
+++ main test.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-
+  double x = 0.0;
+  double y = 0.0;
+  double z = 0.0;
+  double threshhold = 0.0;
+  bool potentialfall = false;
+  
+  intake;
+  if (sqrt((x^2)+(y^2)+(z^2)) >threshhold)
+  {
+    potentialfall = true;
+    if (
+  }
+  
 }
